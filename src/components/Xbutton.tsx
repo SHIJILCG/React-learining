be5e7da420822563e0ba5bf6234b75938b146391
Shijil <shijilcg@gmail.com>
2024-09-11 08:48:17 +0530

    10-09-2024

---
+++ src/components/Xbutton.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+function Xbutton() {
+  return (
+    <div>
+        <button className="border-1 p-2.5 border-black font-bold">X</button>
+        <button className="border-1 p-2.5 border-black font-bold">X</button>
+        <button className="border-1 p-2.5 border-black font-bold">X</button>
+    </div>
+  )
+}
+
+export default Xbutton
